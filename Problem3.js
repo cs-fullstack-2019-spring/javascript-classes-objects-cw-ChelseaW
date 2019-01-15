32f5da4d7264aba56b92670cfa75f74316ffa6c7
ChelseaW <c.winfrey93@yahoo.com>
2019-01-15 13:04:18 -0600

    Objects

---
+++ Problem3.js
@@ -0,0 +1,31 @@
+// Create a Person class with three attributes: name, weight, and height.
+//     Create a method that can calculate the BMI of a person using the formula below.
+//     Create an object of the class and use the function.
+// BMI is (weight / (height * height)) x 703. Weight is in pounds and height is in inches.
+
+
+    class Person
+constructor (name, weight, height)
+{
+    {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+
+    }
+    usingTheFormula(human)
+    {
+       this.weight = human;
+    }
+
+}
+
+var usingTheFormula = new Person(parseInt(BMI is (weight / (height * height))*703););
+usingTheFormula.name();
+usingTheFormula.weight();
+usingTheFormula.height();
+
+
+
+
+
---
+++ Problem4.js
@@ -0,0 +1,27 @@
+// Create a Bank class with the the name and balance attributes.
+//     Afterwards, create an object of the class and follow the instructions below:
+//
+//     Creates an account named "Matt's account" with the balance of 1000
+// Creates an account named "My account" with the balance of 0
+// Withdraws 100.0 from Matt's account
+// Deposits 100.0 to My account
+// Prints both accounts
+
+class Bank
+constructor(500, 550, 600, 650)
+{
+    {
+        this.500 = 500;
+        this.550 = 550;
+        this.600 = 600;
+        this.650 = 650;
+    }
+
+    {
+
+    }
+
+
+}
+var money = new Bank
+money
